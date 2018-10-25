c3c2b142c6b541b2c7a1e7afb4cb7efb3747ae10
Rinat Kashapov <rinatkzz@yandex.ru>
2018-10-25 22:29:09 +0300

    configure mockjax, add paginator

--- public/js/page_view.js
+++ public/js/page_view.js
@@ -1,29 +1,53 @@
+
+var app = null;
 (function (d, w){
     w.addEventListener('load', function (e) {
         init();
     });
-
-    var routes = {
     
-    }
-
+    
     function init() {
-        var app = new Vue({
-            el: "#itemsContainer",
-            data: {
-                items: [
-                    {"id": "1", "type": "movie", "category": "трейлер", "pageUrl": "/movie/1", "name": "Железный Человек 1 / Iron Man 1"},
-                    {"id": "2", "type": "movie", "category": "трейлер", "pageUrl": "/movie/2", "name": "Железный Человек 2 / Iron Man 2"},
-                    {"id": "3", "type": "movie", "category": "трейлер", "pageUrl": "/movie/3", "name": "Железный Человек 3 / Iron Man 3"},
-                    {"id": "4", "type": "movie", "category": "трейлер", "pageUrl": "/movie/4", "name": "Железный Человек 4 / Iron Man 4"},
-                    {"id": "5", "type": "movie", "category": "трейлер", "pageUrl": "/movie/5", "name": "Железный Человек 5 / Iron Man 5"},
-                    {"id": "6", "type": "movie", "category": "трейлер", "pageUrl": "/movie/6", "name": "Железный Человек 6 / Iron Man 6"},
-                    {"id": "7", "type": "movie", "category": "трейлер", "pageUrl": "/movie/7", "name": "Железный Человек 7 / Iron Man 7"},
-                    {"id": "8", "type": "movie", "category": "трейлер", "pageUrl": "/movie/8", "name": "Железный Человек 8 / Iron Man 8"},
-                    {"id": "9", "type": "music", "genre": "rock", "pageUrl": "/music/13", "name": "Limp Bizkit ­ Behind Blue Eyes"}
-                ],
-                currentRoute: w.location.pathname
+        
+        $.mockjax(function (settings) {
+            var page = settings.url.match(/\/\?page=(\d)$/);
+            if (page) {
+                return {
+                    proxy: "/mocks/" + page[1] + ".json"
+                };
             }
         });
+        d.getElementById('itemsContainer').style="";
+        var url = new URL(w.location.href);
+        var page = url.searchParams.get('page');
+
+        if (!page)
+            page = 1;
+
+        app = new Vue({
+            el: "#itemsContainer",
+            data: {
+                items: [],
+                error: false,
+                pages: []
+            }
+        });
+
+        $.ajax({
+            url: '/data/?page=' + page,
+            success: function (data) {
+                data = JSON.parse(data);
+                if (data.hasOwnProperty('content')) {
+                    app.items = data['content'];
+                    if (data.hasOwnProperty('lastPage')) {
+                        app.pages = Array.from({length: parseInt(data.lastPage)}, (v, k) => k+1)
+                    }
+                }
+            },
+            error: function (e) {
+                app.error = true;
+            }
+        });
+
+        
     }
 })(document, window);
